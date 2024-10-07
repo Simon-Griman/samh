4e85d0cec57e4e4cc2f7959f11f5f65034fc609c
Simon Griman <rayoysombra@gmail.com>
2024-10-07 15:01:33 -0400

    actualizacion de pdf misEquipos

--- app/Http/Controllers/MiEquipoController.php
+++ app/Http/Controllers/MiEquipoController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Equipo;
+use App\Models\Ubicacion;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,7 +20,9 @@ class MiEquipoController extends Controller
     {
         $user = Auth::User();
 
-        $misEquipos = Equipo::select('equipos.id', 'tipoequipos.id as id_tipo', 'tipoequipos.nombre as equipo', 'marcas.id as id_marca', 'marcas.nombre as marca', 'modelos.id as id_modelo', 'modelos.nombre as modelo', 'serial', 'bien_nacional', 'rolequipos.id as id_rol', 'rol', 'departamentos.id as id_departamento', 'departamentos.nombre as departamento', 'users.id as id_user', 'name')
+        $ubicacion = Ubicacion::find($user->ubicacion_id);
+
+        $misEquipos = Equipo::select('equipos.id', 'tipoequipos.id as id_tipo', 'tipoequipos.nombre as equipo', 'marcas.id as id_marca', 'marcas.nombre as marca', 'modelos.id as id_modelo', 'modelos.nombre as modelo', 'serial', 'bien_nacional', 'rolequipos.id as id_rol', 'rol', 'departamentos.id as id_departamento', 'departamentos.nombre as departamento', 'users.id as id_user', 'name', 'fecha_adquisicion')
             ->join('tipoequipos', 'tipoequipos.id', '=', 'equipos.tipoequipo_id')
             ->join('marcas', 'marcas.id', '=', 'equipos.marca_id')
             ->join('modelos', 'modelos.id', '=', 'equipos.modelo_id')
@@ -37,7 +41,11 @@ class MiEquipoController extends Controller
                 break;
             }
 
-            $pdf = Pdf::loadView('pdfs.mis-equipos', ['misEquipos' => $misEquipos, 'user' => $user, 'departamento' => $departamento])->setOptions(['defaultFont' => 'arial', 'isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);
+            $item = 1;
+
+            $fecha = Carbon::now()->format('d/m/Y');
+
+            $pdf = Pdf::loadView('pdfs.mis-equipos', ['misEquipos' => $misEquipos, 'user' => $user, 'departamento' => $departamento, 'fecha' => $fecha, 'item' => $item, 'ubicacion' => $ubicacion->nombre])->setOptions(['defaultFont' => 'arial', 'isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);
 
             return $pdf->download('mis-equipos.pdf');
         }
--- resources/views/pdfs/mis-equipos.blade.php
+++ resources/views/pdfs/mis-equipos.blade.php
@@ -14,65 +14,132 @@
 <body>
 
     <?php 
-        $imagen = 'storage/profile-photos/cintillo_osti.jpg';
+        $imagen = 'storage/profile-photos/cintillo_ayacucho.jpg';
         $base64 = base64_encode(file_get_contents($imagen));
     ?>
-
-    <div class="container">
-        <div class="row d-flex justify-content-center">
+    <div class=""></div>
+        <header>
             <img src="data:image/png;base64,<?php echo $base64 ?>" class="w-100">
-            <div class="text-center m-4">
-                <h3>Control de Inventario por Ubicación y Usuario</h3>
-            </div>
-            <div class="card mt-2">
-                <div class="card-body">
+        </header>
 
-                    <table class="table table-responsive table-hover tabla">
-                        <thead>
-                            <tr>
-                                <th class="gris">Usuario</th>
-                                <th>{{ $user->name }}</th>
-                                <th>C.I. {{ $user->cedula }}</th>
-                                <th class="gris">Ubicación</th>
-                                <th>{{ $departamento }}</th>
-                            </tr>
-                            <tr>
-                                <th class="gris">Nombre</th>
-                                <th class="gris">Marca</th>
-                                <th class="gris">Modelo</th>
-                                <th class="gris">Serial</th>
-                                <th class="gris">Bien Nacional</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($misEquipos as $miEquipo)
-                            <tr>
-                                <td>{{ $miEquipo->equipo }}</td>
-                                <td>{{ $miEquipo->marca }}</td>
-                                <td>{{ $miEquipo->modelo }}</td>
-                                <td>{{ $miEquipo->serial }}</td>
-                                @if ($miEquipo->bien_nacional >= 1000)
-                                <td>0000{{ $miEquipo->bien_nacional }}</td>
-                                @else
-                                <td>00000{{ $miEquipo->bien_nacional }}</td>
-                                @endif
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <br><br><br>
-            <table class="table" id="linea">
-                <tr>
-                    <td class="yes text-center">Area de Bienes Nacionales</td>
-                    <td class="none"></td>
-                    <td class="yes text-center"> Firma del Usuario</td>
-                </tr>
-            </table>
-            <br>
-            <small><b>NOTA:</b> EL FUNCIONARIO ES RESPONSABLE DEL CUIDADO Y RESGUARDO DE LOS BIENES NACIONALES AQUI SEÑALADOS</small>
+        <table class="tabla table table-responsive tabla1">
+            <tr>
+                <td colspan="3" class="text-center title"><strong>FICHA DE ASIGNACIÓN DE BIENES PÚBLICOS A RESPONSABLE DIRECTO Y FINAL DE USO</strong></td>
+            </tr>
+            <tr>
+                <td><strong>ORGANISMO</strong></td>
+                <td class="text-center"><strong>SERVICIO AUTONIOMO DE METROLOGIA DE HIDROCARBUROS</strong></td>
+                <td>FECHA:</td>
+            </tr>
+            <tr>
+                <td><strong>UNIDAD ADM. ESTRUCTURAL:</strong></td>
+                <td class="text-center">{{ $departamento }}</td>
+                <td class="gris">{{ $fecha }}</td>
+            </tr>
+            <tr>
+                <td class="gris"><strong>RESPONSABLE ADMIN 1:</strong></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>CÉDULA DE IDENTIDAD:</strong></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>CARGO:</strong></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>UNIDAD ADM. FUNCIONAL:</strong></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="gris"><strong>RESPONSABLE ADMIN 2:</strong></td>
+                <td>{{ $user->name }}</td>
+                <td class="gris">ACTIVO: (X)</td>
+            </tr>
+            <tr>
+                <td><strong>CÉDULA DE IDENTIDAD:</strong></td>
+                <td>{{ $user->cedula }}</td>
+                <td class="gris">PASIVO: ( )</td>
+            </tr>
+            <tr>
+                <td><strong>CARGO:</strong></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>UBICACIÓN GEOGRÁFICA</strong></td>
+                @if ($ubicacion == 'Maracaibo')
+                <td class="gris text-center"><strong>Av.5 de Julio con calle 71 edificio  Minpet Maracaibo estado Zulia</strong></td>
+                @else
+                <td class="gris">{{ $ubicacion }}</td>
+                @endif
+                <td></td>
+            </tr>
+        </table>
+
+        <div class="text-center">
+            <p class="sub-title"><strong>DESCRIPCIÓN DEL BIEN</strong></p>
         </div>
+
+        <div class="card">
+            <div class="card-body">
+
+                <table class="table table-responsive table-hover tabla tabla2">
+                    <thead>
+                        <tr>
+                            <th>ÍTEM</th>
+                            <th>N° DE BIEN PÚBLICO</th>
+                            <th>DESCRIPCIÓN</th>
+                            <th>MARCA</th>
+                            <th>MODELO</th>
+                            <th>SERIAL</th>
+                            <th>ESTADO</th>
+                            <th>FECHA DE ADQUISICIÓN</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($misEquipos as $miEquipo)
+                        <tr>
+                            <td>{{ $item++ }}</td>
+                            @if ($miEquipo->bien_nacional >= 1000)
+                            <td>0000{{ $miEquipo->bien_nacional }}</td>
+                            @else
+                            <td>00000{{ $miEquipo->bien_nacional }}</td>
+                            @endif
+                            <td>{{ $miEquipo->equipo }}</td>
+                            <td>{{ $miEquipo->marca }}</td>
+                            <td>{{ $miEquipo->modelo }}</td>
+                            <td>{{ $miEquipo->serial }}</td>
+                            <td>{{ $miEquipo->rol }}</td>
+                            <td>{{ $miEquipo->fecha_adquisicion }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <p class="justy">Cabe destacar la importancia y deber de cada trabajador de mantener, conservar y proteger los bienes nacionales de conformidad con lo establecido en el decreto Nro. 1.407, con Rango Valor y Fuerza de Ley Orgánica de Bienes Públicos, de fecha 13 de noviembre del año 2014, publicado en Gaceta Oficial Extraordinaria Nro. 6.155 de fecha 19 de noviembre del año 2014. De igual forma se les recuerda que los responsables de los bienes nacionales deben velar por la conservación y buen uso de los mismos evitando así sanciones penales, civiles, administrativas y disciplinarias establecidas en la referida Ley</p>
+        <br><br><br>
+        <table class="table" id="linea">
+            <tr>
+                <td class="yes text-center">
+                    <small class="responsable">RESPONSABLE FINAL DE USO</small>
+                    <p class="nombre"><strong>{{ mb_strtoupper($user->name) }}</strong></p>
+                    <p class="cedula"><strong>{{ $user->cedula }}</strong></p>
+                </td>
+                <td class="none"></td>
+                <td class="yes text-center">
+                    <small class="responsable">RESPONSABLE PATRIMONIAL SAMH</small>
+                    <p class="nombre"><strong>DIANORAH VALERO</strong></p>
+                    <p class="cedula"><strong>7965185</strong></p>
+                </td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
